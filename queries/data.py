c8f3b8f7d31a36ae7e0961d4d35e56d9e5a884fe
Ezequiel Saldaña <eze2286@hotmail.com>
2022-06-02 20:27:53 -0300

    change code

--- queries/data.py
+++ queries/data.py
@@ -4,7 +4,8 @@ import json
 
 API_key = "ak_InwypYyuGx9GQlWNUHDf"
 alphacast = Alphacast(API_key)
-df_bonos_iamc = alphacast.datasets.dataset(7961).download_data("pandas").iloc[:,:-1]
+# df_bonos_iamc = alphacast.datasets.dataset(7961).download_data("pandas").iloc[:,:-1]
+df_bonos_iamc = alphacast.datasets.dataset(7961).download_data("pandas")
 df_bonos_iamc_json = alphacast.datasets.dataset(7961).download_data("json")
 df_pesos = df_bonos_iamc[df_bonos_iamc["Moneda"]=="pesos"]
 df_dolares = df_bonos_iamc[df_bonos_iamc["Moneda"]=="dolares"]
