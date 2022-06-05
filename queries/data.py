9f653f5daa9771acdf4a50d3363ed0fcc6d25ece
Ezequiel Saldaña <eze2286@hotmail.com>
2022-06-05 01:20:30 -0300

    change code

--- queries/data.py
+++ queries/data.py
@@ -9,10 +9,10 @@ API_key = str(key)
 alphacast = Alphacast(API_key)
 # df_bonos_iamc = alphacast.datasets.dataset(7961).download_data("pandas").iloc[:,:-1]
 df_bonos_iamc = alphacast.datasets.dataset(7961).download_data("pandas")
-
+# df_bonos_iamc_json = alphacast.datasets.dataset(7961).download_data("json")
+df_bonos_iamc_json = df_bonos_iamc.to_json()
 df_bonos_iamc["year"] = df_bonos_iamc["Fecha"].apply(lambda x: pd.to_numeric(x[:4]))
 
-df_bonos_iamc_json = alphacast.datasets.dataset(7961).download_data("json")
 df_pesos = df_bonos_iamc[df_bonos_iamc["Moneda"]=="pesos"]
 df_dolares = df_bonos_iamc[df_bonos_iamc["Moneda"]=="dolares"]
 list_tickers = list(df_bonos_iamc["Codigo"].drop_duplicates())
